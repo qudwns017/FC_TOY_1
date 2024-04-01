f56db3593e6fffe85b80a080c56245a6a8cc089e
qudwns017 <qudwns017@naver.com>
2024-04-01 20:33:44 +0900

    Feat: ItineraryController addItinerary 메서드 추가

--- src/main/java/controller/ItineraryController.java
+++ src/main/java/controller/ItineraryController.java
@@ -9,6 +9,32 @@ import java.util.Objects;
 import src.main.java.model.Itinerary;
 
 public class ItineraryController {
+    Gson gson = new Gson();
+    Date date = new Date();
+
+    public void addItinerary(int tripId) {
+        if (checkExistFile(tripId, "trip") == -1) {
+            System.out.println("여행이 존재하지 않습니다.");
+            return;
+        }
+        if (checkExistFile(tripId, "itinerary") == -1) { // 유효한 tripId지만 입력된 여정이 없을 때
+                int itineraryId = tripId;
+                Itinerary itinerary = new Itinerary(itineraryId, "A", "B", date, date);
+                String fileName = "src/main/resources/itinerary/itinerary_" + itinerary.getItinerary_id() + ".json";
+                try {
+                    FileWriter fw = new FileWriter(fileName);
+                    gson.toJson(itinerary, fw);
+                    fw.flush();
+                    fw.close();
+                    System.out.println("작성 완료");
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+        } else {
+            // 이어쓰기 필요
+        }
+    }
+
     public int checkExistFile(int tripId, String travelType) { // id 기준 json이 존재한다면 tripId 반환, 없으면 0 반환
         String dir = "src/main/resources/" + travelType + "/";
         File file = new File(dir);
