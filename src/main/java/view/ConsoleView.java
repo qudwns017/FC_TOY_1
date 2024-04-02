42b22b9b08955f24f34351e0a5793fba11a19cff
fullm <matoma1_@naver.com>
2024-04-02 19:45:18 +0900

    Feat: getTripInfo() 여행 기록 메서드 추가

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -1,5 +1,6 @@
 package src.main.java.view;
 
+import src.main.java.model.Trip;
 import src.main.java.utils.Messages;
 
 import java.text.SimpleDateFormat;
@@ -11,4 +12,26 @@ public class ConsoleView {
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
     SimpleDateFormat TimeFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 
+    public Trip getTripInfo() { //1.여행 기록
+        messages.equal();
+        System.out.println("- 여행 이름을 입력해주세요. :");
+        String tripName = sc.nextLine();
+        System.out.println("- 시작 날짜를 입력해주세요. ex) 20240401 :");
+        String startDateFormat = sc.nextLine();
+        System.out.println("- 종료 날자를 입력해주세요. ex) 20240403 :");
+        String endDateFormat = sc.nextLine();
+        System.out.println();
+
+        Date startDate;
+        Date endDate;
+
+        try { //문자열을 데이트 포맷으로 변경
+            startDate = dateFormat.parse(startDateFormat);
+            endDate = dateFormat.parse(endDateFormat);
+        } catch (Exception e) {
+            System.out.println("잘못된 형식입니다. yyyymmdd 형식으로 입력해주세요");
+            return null;
+        }
+        return new Trip(tripName, startDate, endDate);
+    }
 }
