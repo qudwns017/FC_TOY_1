805a0603b6320b3463063bd382fe401169e5180e
jisun.park <pjs981114@naver.com>
2024-04-02 15:46:54 +0900

    REFACTOR 변수명 정리 및 메인에 적용

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,9 @@
 package src.main.java;
 
-import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
 import src.main.java.dto.TripSub;
-import src.main.java.model.Itinerary;
 import src.main.java.model.Trip;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,8 +14,8 @@ public class main {
         TripController tripController = new TripController();
         tripController.addTrip(trip);
 
-        System.out.println(TripController.readTrip(1).toString());
-        ArrayList<TripSub> sub = TripController.readTripAll();
+        System.out.println(TripController.getTrip(1).toString());
+        ArrayList<TripSub> sub = TripController.getAllTrip();
         for (int i = 0; i < sub.size(); i++) {
             System.out.println(sub.get(i).toString());
         }
