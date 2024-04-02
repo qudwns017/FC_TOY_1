7693a90dd329b056e6657ef3f0e60978af4216c5
fullm <matoma1_@naver.com>
2024-04-02 20:05:14 +0900

    Feat: 객체 생성

---
+++ src/main/java/view/TravelApp.java
@@ -0,0 +1,15 @@
+package src.main.java.view;
+
+import src.main.java.controller.ItineraryController;
+import src.main.java.controller.TripController;
+import src.main.java.utils.Messages;
+
+public class TravelApp {
+    public static void main(String[] args) {
+        ItineraryController itineraryController = new ItineraryController();
+        TripController tripController = new TripController();
+        src.main.java.ConsoleView consoleView = new src.main.java.ConsoleView();
+        Messages messages = new Messages();
+
+    }
+}
