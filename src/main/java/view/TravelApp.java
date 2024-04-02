7a76137979586e743accfb522c496016aea43f7a
fullm <matoma1_@naver.com>
2024-04-02 20:08:25 +0900

    Feat:t시작 화면 출력, 메뉴 입력 받기

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -4,6 +4,8 @@ import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
 import src.main.java.utils.Messages;
 
+import java.util.Scanner;
+
 public class TravelApp {
     public static void main(String[] args) {
         ItineraryController itineraryController = new ItineraryController();
@@ -11,5 +13,10 @@ public class TravelApp {
         src.main.java.ConsoleView consoleView = new src.main.java.ConsoleView();
         Messages messages = new Messages();
 
+        while (num){
+            messages.startApp();
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+        }
     }
 }
