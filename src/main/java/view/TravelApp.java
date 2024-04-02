2b6c9401f9ee7f03c5186db0a652bc94ddf84b56
fullm <matoma1_@naver.com>
2024-04-02 20:13:40 +0900

    Feat: 5. 잘못된 입력값

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -59,6 +59,9 @@ public class TravelApp {
                 case 4:
                     System.out.println(" 프로그램을 종료합니다. ");
                     return;
+                default:
+                    System.out.println(" 잘못된 입력입니다. ");
+                    break;
 
             }
         }
