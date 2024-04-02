97df41a29387b62af455b80fe42a711457ba74bd
fullm <matoma1_@naver.com>
2024-04-02 20:09:59 +0900

    Feat 1.여행기록

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -2,6 +2,7 @@ package src.main.java.view;
 
 import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
+import src.main.java.model.Trip;
 import src.main.java.utils.Messages;
 
 import java.util.Scanner;
@@ -13,10 +14,19 @@ public class TravelApp {
         src.main.java.ConsoleView consoleView = new src.main.java.ConsoleView();
         Messages messages = new Messages();
 
-        while (num){
+        while (true){
             messages.startApp();
             Scanner sc = new Scanner(System.in);
             int num = sc.nextInt();
+
+            switch (num){
+                case 1: //여행 기록
+                    Trip newTrip = consoleView.getTripInfo();
+                    tripController = addTrip(newTrip);
+                    System.out.println("여행이 추가되었습니다. ");
+                    break;
+
+            }
         }
     }
 }
