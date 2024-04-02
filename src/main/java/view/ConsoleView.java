4d806467cd306ce3300dd70cc595153d8cd9ab9d
fullm <matoma1_@naver.com>
2024-04-02 19:39:29 +0900

    Feat: 객체 생성

---
+++ src/main/java/view/ConsoleView.java
@@ -0,0 +1,14 @@
+package src.main.java.view;
+
+import src.main.java.utils.Messages;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class ConsoleView {
+    Messages messages = new Messages();
+    Scanner sc = new Scanner(System.in);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+    SimpleDateFormat TimeFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+
+}
