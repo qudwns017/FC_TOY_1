cbccdc6feb45315904a00bccf2d2416eb0ab701a
fullm <matoma1_@naver.com>
2024-04-03 16:14:08 +0900

    Feat: 여정/여행기록 뷰로 이동

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -2,31 +2,27 @@ package src.main.java.view;
 
 import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
+import src.main.java.model.Itinerary;
 import src.main.java.model.Trip;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class MainView {
-    public static void main(String[] args) {
         AddTripView addTripView = new AddTripView(); //여행 기록 뷰
         AddItineraryView addItineraryView = new AddItineraryView(); //여정 기록 뷰
         GetTravelView getTravelView = new GetTravelView(); // 여정/여행 조회 뷰
         ItineraryController itineraryController = new ItineraryController();
         TripController tripController = new TripController();
-
-        while (true){
-            Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-
-            switch (num){
-                case 1:
-                    Trip newTrip = addTripView.getTripInfo();
-                    tripController.addTrip(newTrip);
-                    System.out.println("여행이 추가되었습니다. ");
-                    break;
-
-            }
+        public AddTripView getAddTripView(){
+                return addTripView;
         }
-    }
 
+        public AddItineraryView getAddItineraryView() {
+                return addItineraryView;
+        }
+
+        public GetTravelView getGetTravelView(){
+                return getTravelView;
+        }
 }
