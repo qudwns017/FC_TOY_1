53a498ee9dd4c4dd9ab52835aa5216bcc85cfe11
cbjoon <qudwns017@naver.com>
2024-04-03 12:26:56 +0900

    Feat: addTrip 메서드에 여행 id 추가

--- src/main/java/controller/TripController.java
+++ src/main/java/controller/TripController.java
@@ -3,6 +3,7 @@ package src.main.java.controller;
 import src.main.java.model.Trip;
 
 import java.util.List;
+import src.main.java.utils.JsonConverter;
 
 public class TripController {
 
@@ -11,6 +12,8 @@ public class TripController {
     public void addTrip(Trip trip) {
         // 출발일 <= 도착일 확인하기
         // 같은 이름의 같은 일정으로 여행이 있는지 확인하기
+        List<Trip> list = jsonConverter.loadAllTrip();
+        trip.setTripId(list.size() + 1);
         jsonConverter.saveTrip(trip);
     }
 
