baeb7169b56d6d115d8cfd6dca7d93fc053f4a52
fullm <matoma1_@naver.com>
2024-04-03 15:34:52 +0900

    Feat: 컨트롤러 생성

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,5 +1,9 @@
 package src.main.java.view;
 
+import src.main.java.controller.ItineraryController;
+import src.main.java.controller.TripController;
+import src.main.java.model.Trip;
+
 import java.util.Scanner;
 
 public class MainView {
@@ -7,5 +11,10 @@ public class MainView {
         AddTripView addTripView = new AddTripView(); //여행 기록 뷰
         AddItineraryView addItineraryView = new AddItineraryView(); //여정 기록 뷰
         GetTravelView getTravelView = new GetTravelView(); // 여정/여행 조회 뷰
+        ItineraryController itineraryController = new ItineraryController();
+        TripController tripController = new TripController();
+
+
     }
+
 }
