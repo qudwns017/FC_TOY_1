04f20bc06c1303978692ed122e6d483626668211
jisun.park <pjs981114@naver.com>
2024-04-03 15:46:41 +0900

    FEAT : ConsoleView에서 addTrip 기능 분리

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -40,28 +40,6 @@ public class ConsoleView {
         return trip_id;
     }
 
-    public Trip getTripInfo() { //1.여행 기록
-        messages.equal();
-        System.out.println("- 여행 이름을 입력해주세요. :");
-        String tripName = sc.nextLine();
-        System.out.println("- 시작 날짜를 입력해주세요. ex) 20240401 :");
-        String startDateFormat = sc.nextLine();
-        System.out.println("- 종료 날자를 입력해주세요. ex) 20240403 :");
-        String endDateFormat = sc.nextLine();
-        System.out.println();
-
-        Date startDate;
-        Date endDate;
-
-        try { //문자열을 데이트 포맷으로 변경
-            startDate = dateFormat.parse(startDateFormat);
-            endDate = dateFormat.parse(endDateFormat);
-        } catch (Exception e) {
-            System.out.println("잘못된 형식입니다. yyyymmdd 형식으로 입력해주세요");
-            return null;
-        }
-        return new Trip(tripName, startDate, endDate);
-    }
 
     public Itinerary getItineraryInfo() { //2.여정기록
         messages.equal();
