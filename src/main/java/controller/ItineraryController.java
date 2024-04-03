226ee2c8c7b2993dc365cefa6d278e414beb6d96
cbjoon <qudwns017@naver.com>
2024-04-03 11:35:12 +0900

    Feat: getItinerary 메서드 추가

--- src/main/java/controller/ItineraryController.java
+++ src/main/java/controller/ItineraryController.java
@@ -1,8 +1,6 @@
 package src.main.java.controller;
 
 import java.util.List;
-import java.util.Objects;
-import src.main.java.model.ItinerariesJsonDTO;
 import src.main.java.model.Itinerary;
 
 public class ItineraryController {
@@ -20,44 +18,14 @@ public class ItineraryController {
         return jsonConverter.loadItineray(tripId);
     }
 
-    public void addItinerary(int tripId, List<Itinerary> itineraries) {
-        itineraries = new ArrayList<>();
-        if (checkExistFile(tripId, "trip") == -1) {
-            System.out.println("여행이 존재하지 않습니다.");
-            return;
-        }
-        Itinerary itinerary = new Itinerary(0, "A", "B", date, date);
-        // list에 기존 객체 추가
-        itineraries.add(itinerary);
-        ItinerariesJsonDTO dto = new ItinerariesJsonDTO(itineraries);
-        String fileName = "src/main/resources/itinerary/itinerary_" + tripId + ".json";
-        try {
-            FileWriter fw = new FileWriter(fileName);
-            gson.toJson(dto, fw); // 변환한 json을 파일로 기록
-            fw.flush();
-            fw.close();
-            System.out.println("작성 완료");
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
-    public int checkExistFile(int tripId, String travelType) { // id 기준 json이 존재한다면 tripId 반환, 없으면 0 반환
-        String dir = "src/main/resources/" + travelType + "/";
-        File file = new File(dir);
-
-        File[] list = file.listFiles();
-        int index = 5;
-        if (Objects.equals(travelType, "itinerary")) {
-            index = 10;
-        }
-
-        for (File t : Objects.requireNonNull(list)) {
-            int jsonFileId = Integer.parseInt(t.getName().substring(index, t.getName().indexOf('.')));
-            if (Objects.equals(jsonFileId, tripId)) {
-                return jsonFileId;
+    public Itinerary getItinerary(int tripId, int itinerary_id) {
+        List<Itinerary> itineraryList = jsonConverter.loadItineray(tripId);
+        for (Itinerary obj : itineraryList) {
+            if (itinerary_id == obj.getItineraryId()) {
+                return obj;
             }
         }
-        return -1;
+        System.out.println("해당하는 여정이 없음");
+        return null;
     }
 }
