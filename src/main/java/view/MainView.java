dc75f23a462e4e18c79eeaaa2e11bcb8ef83b249
fullm <matoma1_@naver.com>
2024-04-03 17:08:39 +0900

    Feat: 시작 메세지 ì출력

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -4,27 +4,33 @@ import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
 import src.main.java.model.Itinerary;
 import src.main.java.model.Trip;
+import src.main.java.utils.Messages;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class MainView {
+        //TravelApp에서 입력 받은 값을 전달 받아야
+        //MainView mainView = new MainView();
+        //mainView.sendView
+        //startmessage,
+        //
         AddTripView addTripView = new AddTripView(); //여행 기록 뷰
         AddItineraryView addItineraryView = new AddItineraryView(); //여정 기록 뷰
         GetTravelView getTravelView = new GetTravelView(); // 여정/여행 조회 뷰
-
+        Messages messages = new Messages();
         public void sendView(int num){
-                switch (num){
-                        case 1:
-                                addTripView.getTripInfo();
-                                break;
-                        case 2:
-                                addItineraryView.getItineraryInfo();
-                                break;
-                        case 3:
-                                getTravelView.printTravelInfo();
-                                break;;
-
+            messages.startApp();
+        switch (num){
+                case 1:
+                       addTripView.getTripInfo();
+                       break;
+                case 2:
+                       addItineraryView.getItineraryInfo();
+                       break;
+                case 3:
+                        getTravelView.printTravelInfo();
+                        break;;
                 }
         }
 }
