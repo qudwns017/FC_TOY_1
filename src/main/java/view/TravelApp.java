0b35bc8b3cd8125a312fb192fa61920aa33f1181
fullm <matoma1_@naver.com>
2024-04-04 11:30:28 +0900

    Feat: 조종료와 잘못된 입력

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -22,7 +22,15 @@ public class TravelApp {
                 case 2:
                     mainView.addItineraries();
                     break;
-
+                case 3:
+                    mainView.showItineraries();
+                    break;
+                case 4:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다. ");
+                    break;
             }
         }
     }
