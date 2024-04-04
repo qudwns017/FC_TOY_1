e0f7bc28d58ffaeba0fb11943ca9f3a9d932499c
jisun.park <pjs981114@naver.com>
2024-04-04 18:20:35 +0900

    FEAT : clear 기능 추가

--- src/main/java/utils/Messages.java
+++ src/main/java/utils/Messages.java
@@ -85,4 +85,10 @@ public class Messages {
             e.getStackTrace();
         }
     }
+
+    public static void clear()
+    {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
