896d71a1b2ca4ba4ddbd8f8e7f1b20aa280cb697
GOren98 <gangmin9812@gmail.com>
2024-04-04 17:31:33 +0900

    Modify : Main의 필요없는 import 제거

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -1,15 +1,5 @@
 package src.main.java.view;
 
-import com.sun.tools.javac.Main;
-import src.main.java.controller.ItineraryController;
-import src.main.java.controller.TripController;
-import src.main.java.model.Itinerary;
-import src.main.java.model.Trip;
-import src.main.java.utils.Messages;
-
-import java.util.List;
-import java.util.Scanner;
-
 public class TravelApp {
     public static void main(String[] args) {
         MainView mainView = new MainView();
