c8ade33e6eda21273d9fd5874b340a244f33baae
fullm <matoma1_@naver.com>
2024-04-04 11:28:58 +0900

    Feat:여행 추가

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -11,58 +11,23 @@ import java.util.Scanner;
 
 public class TravelApp {
     public static void main(String[] args) {
-        ItineraryController itineraryController = new ItineraryController();
-        TripController tripController = new TripController();
-        src.main.java.ConsoleView consoleView = new src.main.java.ConsoleView();
-        Messages messages = new Messages();
+        MainView mainView = MainView();
 
-        while (true){
-            messages.startApp();
-            Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-
+        while(true){
+            int num = mainView.sendCase();
             switch (num){
-                case 1: //여행 기록
-                    Trip newTrip = consoleView.getTripInfo();
-                    tripController = addTrip(newTrip);
-                    System.out.println("여행이 추가되었습니다. ");
+                case 1:
+                    mainView.addTrips();
                     break;
 
-                case 2: //여정기록
-                    Itinerary newItinerary = consoleView.getItineraryInfo();
-                    if (addItinerary != null){
-                        TripController.addItinerary(newItinerary);
-                        System.out.println("추가 완료 했습니다");
-                    }else{
-                        System.out.println("추가에 실패했습니다.");
-                    }
-                    break;
-
-                case 3: //조회
-                    Trip trip = TripController.getAllTrip(); //여행 번호와 아이디만 findAllTrip()
-                    System.out.println("조회할 여행 번호를 입력해주세요 : ");
-                    // 여행 상세 추가 printAllTrip()
-                    try{
-                        trip_id = consoleView.getTrip();
-                        if(getTrip == null){
-                            System.out.println("존재하지 않는 여행 번호 입니다. ");
-                            return;
-                        }
-
-                        List<Itinerary> itineraries = itineraryController.findOneItinerary(trip_id);
-                        trip = TripController.findOneTrip(trip_id);
-                        consoleView.printItineraryInfo(trip,itineraries);
-                        break;
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                case 4:
-                    System.out.println(" 프로그램을 종료합니다. ");
-                    return;
-                default:
-                    System.out.println(" 잘못된 입력입니다. ");
-                    break;
             }
         }
     }
 }
+
+/*
+public int sendCase()
+public void addTrips()
+public void addItineraries()
+public void  showItineraries()
+ */
