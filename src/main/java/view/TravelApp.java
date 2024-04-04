8251367843f37085a0649e0d6031b754535b6afa
fullm <matoma1_@naver.com>
2024-04-04 16:05:37 +0900

    Feat: 각 뷰에 보내줌

--- src/main/java/view/TravelApp.java
+++ src/main/java/view/TravelApp.java
@@ -1,5 +1,6 @@
 package src.main.java.view;
 
+import com.sun.tools.javac.Main;
 import src.main.java.controller.ItineraryController;
 import src.main.java.controller.TripController;
 import src.main.java.model.Itinerary;
@@ -10,4 +11,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TravelApp {
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+
+        while(true){
+            int num = mainView.sendCase();
+            switch (num){
+                case 1:
+                    mainView.addTrips();
+                    break;
+                case 2:
+                    mainView.addItineraries();
+                    break;
+                case 3:
+                    mainView.showItineraries();
+                    break;
+                case 4:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다. ");
+                    break;
+            }
+        }
+    }
 }
