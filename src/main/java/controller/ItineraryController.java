d3728cb042dc2e1deb9c689256a34b0a807a8799
cbjoon <qudwns017@naver.com>
2024-04-04 15:27:57 +0900

    Fix: addItinerary json 파일 추가 오류 해결

--- src/main/java/controller/ItineraryController.java
+++ src/main/java/controller/ItineraryController.java
@@ -1,5 +1,6 @@
 package src.main.java.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import src.main.java.model.Itinerary;
 import src.main.java.utils.JsonConverter;
@@ -7,10 +8,25 @@ import src.main.java.utils.JsonConverter;
 public class ItineraryController {
     JsonConverter jsonConverter = new JsonConverter();
 
+    // 파일이 있을 때 / 없을 때
+    // 있을 때
+    // json파일을 읽어서 list에 삽입
+    // 새로 받은 객체를 리스트의 마지막에 추가하여 json에 다시 save
+
+    // 없을 때
+    // 새로 받은 객체를 바로 save에 넣기
+
     public void addItinerary(int tripId, Itinerary itinerary) {
-        List<Itinerary> itineraryList = getItinerary(tripId);
-        int itineraryId = itineraryList.size() + 1;
-        itinerary.setItineraryId(itineraryId);
+        int itineraryId;
+        List<Itinerary> itineraryList;
+        if (getItinerary(tripId) != null) {
+            itineraryList = getItinerary(tripId);
+            itineraryId = itineraryList.get(itineraryList.size() - 1).getItineraryId() + 1;
+            itinerary.setItineraryId(itineraryId);
+        } else {
+            itineraryList = new ArrayList<>();
+            itinerary.setItineraryId(1);
+        }
         itineraryList.add(itinerary);
         jsonConverter.saveItineraries(tripId, itineraryList);
     }
